e46ab4bda4ac52fa86c259f2303fd9f162f020d3
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-09-04 13:10:09 +0300

    create a test case to check if our application can save a user object

--- password_test.py
+++ password_test.py
@@ -18,5 +18,13 @@ class TestClass(unittest.TestCase):
     self.assertEqual(self.new_user.username, 'ikinoti')
     self.assertEqual(self.new_user.password, '12qwerty34')
 
+  def test_save_user_details(self):
+    '''
+    test if the user objec is saved in the user list
+    '''
+
+    self.new_user.save_user()
+    self.assertEqual(len(User.user_list), 1)
+
 if __name__ == '__main__':
   unittest.main()
