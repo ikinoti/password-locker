f34101ea1ae680f7dc7a2702c71cd62ad09c4550
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-09-04 13:07:38 +0300

    add test case to check if our object is initiated correctly

--- password_test.py
+++ password_test.py
@@ -10,4 +10,13 @@ class TestClass(unittest.TestCase):
     method to run before each test case
     '''
     self.new_user = User('ikinoti', '12qwerty34')
-    
+
+  def test_init(self):
+    '''
+    test case to test if the object is initialized properly
+    '''
+    self.assertEqual(self.new_user.username, 'ikinoti')
+    self.assertEqual(self.new_user.password, '12qwerty34')
+
+if __name__ == '__main__':
+  unittest.main()
