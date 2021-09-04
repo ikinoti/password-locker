8b0b321a1972f6a45ba36e58f9eb9982165c6d61
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-09-05 01:22:23 +0300

    add a method to create a new user and a welcoming message when our app runs

---
+++ run.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3.8
+
+from password import User, Credentials
+
+print('Hello, What is your name')
+name = input('Enter you Name: ')
+print("\n")
+
+print(f"Hey {name}, Welcome to Password locker App")
+print("\n")
+
+def create_newUser(username, password):
+  '''
+  Method to create a new user
+  '''
+  new_user = User(username, password)
+  return new_user
+
+
